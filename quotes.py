66a93aa22f6856047a5c829f7164fccc36972993
Annie Vu <dv2ku@virginia.edu>
2021-04-11 17:22:40 -0400

    v1.1 update commands, add magic 8ball, new lines; README updated, release notes added

--- quotes.py
+++ quotes.py
@@ -1,20 +1,21 @@
 # base file from morgan @GoudaMorganing
 
-quote_about = [
-	"I'm No. 11 of the Fatui Harbingers, codename Childe, but I also go by Tartaglia. And you... Hmm, you too like to cause quite the stir, don't you? Something tells me we're going to get along splendidly.",
-	"If it makes me stronger, I'll take it. Vision? Great. Delusion? Don't mind if I do. Heretical teachings from the Abyss? Sign me up.",
-	"Where I'm from, if we didn't keep moving, we'd freeze to death. Literally.",
-	"Remember, comrade, one day I will conquer the world!",
-	"I once ventured deep into the abyss and came face to face with an enormous beast. I don't know its name, all I know is the sight of it chilled me to the bone. But mark my words, one day I will march back in there and behead that beast, and you, comrade, will be my witness!",
-	"I am the least adept with a bow. And that is precisely why I must master it.",
-	"My pet peeves? Hmm, well, to be honest, I'm far too busy journeying with you and slaying powerful enemies to dwell on such trifling matters.",
-	"However far away my motherland may be, we all live under the same sky. That snow-white moon... My family must be able to see it too. I hope my siblings are well... And I hope that they have turned out to be formidable warriors too.",
-	"It's been years since I last saw my master. I should hope that next we meet, I'll at least be able to force her to use both hands to beat me..."
-]
+quote_help = \
+	("Need some help comrade? Haha alright, just for you...\n\n"
+	"Morning = 'gm'\n"
+	"Evening = 'ge'\n"
+	"Night = 'gn'\n"
+	"Chat = 'ch'\n"
+	"Insult = 'in'\n"
+ 	"Birthday = 'hb'\n"
+	"Choose = 'choose [arg1] | [arg2] | [arg#]'\n"
+	"Rng = 'rng [int1] [int2]'\n"
+	"8ball = 'qq'")
 
 quote_insult = [
 	"This place is pretty dead... By which I mean, there's nothing to kill.",
-	"You know, there are better ways to seek out our enemies than... well, standing here."
+	"You know, there are better ways to seek out our enemies than... well, standing here.",
+	"I'll give you as much mora as you want, so how about we get to sparring? I'm dying of boredom!",
 ]
 
 quote_birthday = [
@@ -31,14 +32,26 @@ quote_morning = [
 
 quote_evening = [
 	"Whew, dinner was quite the feast. A spot of exercise is in order.",
-	"Ah, how time flies. One day, you will see how I conquer the world and crush the thrones of gods beneath my heel. In the meantime... no dying, comrade! Keep yourself alive, haha."
+	"Ah, how time flies. One day, you will see how I conquer the world and crush the thrones of gods beneath my heel. In the meantime... no dying, comrade! Keep yourself alive, haha.",
+	"Evening, comrade! Eaten dinner yet? It won't do to starve yourself. Why don't we get some dinner together? My treat!"
 ]
 
 quote_night = [
-	"Today was great. See you tomorrow, comrade!"
+	"Today was great. See you tomorrow, comrade!",
+	"This late already? I didn't even notice the time. Good night!",
+	"I hope you aren't planning on staying up too late. I need you in top form tomorrow for when we spar!"
 ]
 
 quote_chat = [
+	"I'm No. 11 of the Fatui Harbingers, codename Childe, but I also go by Tartaglia. And you... Hmm, you too like to cause quite the stir, don't you? Something tells me we're going to get along splendidly.",
+	"If it makes me stronger, I'll take it. Vision? Great. Delusion? Don't mind if I do. Heretical teachings from the Abyss? Sign me up.",
+	"Where I'm from, if we didn't keep moving, we'd freeze to death. Literally.",
+	"Remember, comrade, one day I will conquer the world!",
+	"I once ventured deep into the abyss and came face to face with an enormous beast. I don't know its name, all I know is the sight of it chilled me to the bone. But mark my words, one day I will march back in there and behead that beast, and you, comrade, will be my witness!",
+	"I am the least adept with a bow. And that is precisely why I must master it.",
+	"My pet peeves? Hmm, well, to be honest, I'm far too busy journeying with you and slaying powerful enemies to dwell on such trifling matters.",
+	"However far away my motherland may be, we all live under the same sky. That snow-white moon... My family must be able to see it too. I hope my siblings are well... And I hope that they have turned out to be formidable warriors too.",
+	"It's been years since I last saw my master. I should hope that next we meet, I'll at least be able to force her to use both hands to beat me...",
 	"\"A warrior's strength is like a dome. The bigger the dome, the more opponents he can challenge inside it.\" Hmm, who was it that said this again?",
 	"Food is like weaponry. A true warrior never deliberates unduly over their choice of weapon, nor do they do so over the food they eat. Neither should you.",
 	"There's plenty of delicious food to sink your teeth into in this world... and plenty of fights to sink your teeth into as well. Life is beautiful.",
@@ -56,4 +69,31 @@ quote_chat = [
 	"Comrade, how would you like to join the Fatui? ...Actually, scrap that. With your capabilities, you and I could head-to-head with the Eleven and win. Wouldn't want to miss an opportunity like that.",
 	"Ah, I'm sure the Harbingers must know of my dealings with you by now. Oh, I'd love to see the look on their faces... Hahaha...",
 	"You wield such powers without owning a Vision... I really am excited to see what you have in store for us... Let's see how far you can go. Just don't go dying on me before you lay it all on the line in our own final battle! I won't allow it!"
+]
+
+quote_8ball = [
+	"I'll give you this bag of mora, so just do what you need to, comrade.",
+	"Oh, you're asking me comrade? Sure, I don't see why not.",
+	"Without a doubt.",
+	"Is this a trick question? Of course!",
+	"Hmm... As far as I can tell, yes.",
+	"Most likely. ...You seen unconvinced... I'm a bit hurt by this skepticism...",
+	"I think the chances are pretty good - you should have more faith in yourself comrade.",
+	"You seem like you want me to say yes, so let's go with that.",
+	"This seems like the part where I should be saying no, which means the answer is definitely yes.",
+	"Why are you asking me this...? Well, either way I think the outlook is good.",
+
+	"...how about you ask Zhongli instead?",
+	"A-ahaha... How about I tell you later..."
+	"Oh my! Look at the time, I have to go... write to Teucer! See you, comrade!",
+	"... (Pretending not to hear)",
+	"I'm a bit busy, how about you ask me a bit later? ...I'm definitely not running away!",
+	"<:childe_sus_smile:828859134500667412>",
+	"<:childe_oh_no:829471579372388362>",
+
+	"...I hope this is just a joke. Of course not!",
+	"Don't hold your breath.",
+	"Hahah, no.",
+	"I think it'd be best not to hope for much.",
+	"I don't think so, comrade. Even you must realize it's doubtful."
 ]
